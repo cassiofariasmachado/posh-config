ee43a4e4ee6ed5acfead3fd576ef15f5ae9a3a9b
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2021-04-10 11:08:35 -0300

    Adicionar configuração inicial

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+$root = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$localModulesDir = Join-Path $root Modules
+
+Import-Module PSReadline
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler –Key DownArrow -Function HistorySearchForward
+
+Import-Module "$localModulesDir/posh-alias"
+Import-Module "$localModulesDir/posh-git/src/posh-git.psd1"
+
+if (-Not (Get-Module -ListAvailable -Name oh-my-posh)) {
+    Install-Module oh-my-posh -Scope CurrentUser -AllowPrerelease -Force
+}
+
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme "$root/tema.omp.json"
+
+. "$root/CreateAliases.ps1"
