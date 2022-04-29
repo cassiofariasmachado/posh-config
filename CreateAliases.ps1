b10eb9b62aa3f8873a96ea86edfc9264519cb229
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2022-04-29 17:55:45 -0300

    feat: adiciona aliases para o kubectl

--- CreateAliases.ps1
+++ CreateAliases.ps1
@@ -1,3 +1,4 @@
+# Git Aliases
 Add-Alias co 'git checkout'
 Add-Alias gst 'git status'
 Add-Alias grb 'git rebase'
@@ -7,3 +8,17 @@ Add-Alias push 'git push'
 Add-Alias pull 'git pull'
 Add-Alias pushsync 'git push --set-upstream origin HEAD'
 Add-Alias fush 'git push --force-with-lease'
+
+# Kube Controller alias
+Add-Alias kc 'kubectl'
+Add-Alias kca 'kubectl apply'
+Add-Alias kcd 'kubectl delete'
+Add-Alias get-svc 'kubectl get svc'
+Add-Alias get-pods 'kubectl get pods'
+Add-Alias get-nodes 'kubectl get nodes'
+Add-Alias get-cm 'kubectl get cm'
+Add-Alias get-deployments 'kubectl get deployments'
+Add-Alias get-rs 'kubectl get rs'
+Add-Alias get-pv 'kubectl get pv'
+Add-Alias get-pvc 'kubectl get pvc'
+Add-Alias get-sc 'kubectl get sc'
